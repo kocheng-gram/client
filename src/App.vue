b73a74a66e5d4a2a0a45f498ad22cd72694aa610
adityabpradana <adityabpradana@gmail.com>
2019-07-18 16:56:41 +0700

    add vuetify

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+
+    </div>
+</template>
+
+<script>
+import Navbar from './components/navbar'
+export default {
+    data() {
+        return {
+            message: 'Kocheng-Gram'
+        }
+    },
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import Vue from 'vue'
+import App from './App.vue'
 import Axios from 'axios'
+import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Axios)
 Vue.use(Vuetify)
+new Vue(App).$mount('#app')
