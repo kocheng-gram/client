16a5253a8c4d79c40256f693a6bb81859b236f1f
Elia <elia@Elias-MacBook-Air.local>
2019-07-19 10:49:48 +0700

    vision api

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import Axios from 'axios'
+import upload from './components/upload.vue'
+import axios from 'axios'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
+// new Vue(App).$mount('#app');
+new Vue(upload).$mount('#upload');
